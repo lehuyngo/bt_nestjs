fe0c0312599a71d53fd434823591bc011ba48234
Tin <lychanhtintv@gmail.com>
2023-04-04 15:01:10 +0700

    rest api fix post and patch

--- src/runs/runs.controller.ts
+++ src/runs/runs.controller.ts
@@ -9,20 +9,46 @@ import {
 } from '@nestjs/common';
 import { RunsService } from './runs.service';
 import { Run } from './runs.model';
+import {
+  ApiBadRequestResponse,
+  ApiCreatedResponse,
+  ApiBody,
+} from '@nestjs/swagger';
 
 @Controller('runs')
 export class RunsController {
   constructor(private readonly runsService: RunsService) {}
   @Get()
+  @ApiCreatedResponse({
+    description: 'Get all your runs',
+  })
+  @ApiBadRequestResponse({
+    description: 'Cannot get your runs',
+  })
   getAllRun() {
     return this.runsService.getAllRuns();
   }
 
+  @ApiCreatedResponse({
+    description: 'Get your run',
+  })
+  @ApiBadRequestResponse({
+    description: 'Cannot get your run',
+  })
   @Get(':id')
   getRun(@Param('id') id: string) {
     return this.runsService.getRun(id);
   }
 
+  @ApiCreatedResponse({
+    description: 'Create your run',
+  })
+  @ApiBadRequestResponse({
+    description: 'Cannot create your run',
+  })
+  @ApiBody({
+    type: Run,
+  })
   @Post()
   createRun(
     @Body('time') time: number,
@@ -31,11 +57,26 @@ export class RunsController {
     return this.runsService.createRun(description, time);
   }
 
+  @ApiCreatedResponse({
+    description: 'Update all your run',
+  })
+  @ApiBadRequestResponse({
+    description: 'Cannot Update your run',
+  })
+  @ApiBody({
+    type: Run,
+  })
   @Patch(':id')
-  updateRun(@Param('id') id: string, dto: Run) {
+  updateRun(@Param('id') id: string, @Body() dto: Run) {
     return this.runsService.updateRun(id, dto);
   }
 
+  @ApiCreatedResponse({
+    description: 'Delete all your run',
+  })
+  @ApiBadRequestResponse({
+    description: 'Cannot Delete your run',
+  })
   @Delete(':id')
   deleteRun(@Param('id') id: string) {
     return this.runsService.deleteRun(id);
--- src/runs/runs.model.ts
+++ src/runs/runs.model.ts
@@ -1,6 +1,28 @@
 /* eslint-disable prettier/prettier */
-export interface Run {
+import { ApiProperty } from '@nestjs/swagger';
+
+export class Run {
+  @ApiProperty({
+    description: 'Id of your run',
+    minimum: 1,
+    default: 1,
+    type: String,
+  })
   id: string;
+
+  @ApiProperty({
+    description: 'Time of your run',
+    minimum: 10,
+    default: 10,
+    type: Number,
+  })
   time: number;
+
+  @ApiProperty({
+    description: 'Description of your run',
+    minimum: 0,
+    default: "Running",
+    type: String,
+  })
   description: string;
 }
